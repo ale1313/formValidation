96a8a78146910ee96337dc9a352d939cd263e1ec
ale1313 <alejo.vs99@gmail.com>
2019-10-09 11:23:41 -0300

    Minor fixes

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,9 @@
-const presets = [
-  [
-    "@babel/env",
-    {
-      targets: {
-        edge: "17",
-        firefox: "60",
-        chrome: "49",
-        safari: "11.1"
-      },
-      useBuiltIns: "usage"
-    }
-  ]
-];
+module.exports = function(api) {
+  api.cache(true);
 
-module.exports = { presets };
+  const presets = ["@babel/preset-env"];
+
+  return {
+    presets
+  };
+};
